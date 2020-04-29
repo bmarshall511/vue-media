1fe27c6234d035aafb3184db64e0157a82031701
Ben Marshall <bmarshall511@gmail.com>
2020-04-29 18:01:21 -0500

    deploy

---
+++ precache-manifest.6603e1e93639c92f42afce874acd24b1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "188758cea0135377ef4b",
+    "url": "/vue-media/css/app.4393d9dd.css"
+  },
+  {
+    "revision": "40c511360e91add1cdce4d159c2d8027",
+    "url": "/vue-media/img/imdb-logo.40c51136.svg"
+  },
+  {
+    "revision": "d754e6384ff1f2aa92269bde7b8ad3ef",
+    "url": "/vue-media/img/metacritic-logo.d754e638.svg"
+  },
+  {
+    "revision": "4ed44fd415b5149ca8e075b2e94a72b8",
+    "url": "/vue-media/index.html"
+  },
+  {
+    "revision": "188758cea0135377ef4b",
+    "url": "/vue-media/js/app.a5db0432.js"
+  },
+  {
+    "revision": "3f7915d20dbe0ac56aca",
+    "url": "/vue-media/js/chunk-vendors.120fdf71.js"
+  },
+  {
+    "revision": "0dfd50a7e91709a0d03622f3e3ad7a36",
+    "url": "/vue-media/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-media/robots.txt"
+  }
+]);
